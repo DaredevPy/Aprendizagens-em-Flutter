63523b272397918f326966a59f42e1485f69c6ca
DaredevPy <renatopiment@gmail.com>
2026-06-11 11:55:22 -0300

    Fundamentos Dart

---
+++ Variaveis_e_tipos.dart
@@ -0,0 +1,36 @@
+void main () {
+
+  int idade = 38; // Variavel do tipo inteiro
+  double altura = 1.75; // Variavel do tipo decimal
+  String nome = "João"; // Variavel do tipo texto
+  bool estudante = true; // Variavel do tipo booleano
+
+  // Camelcase
+  String nomeCompleto = "Maria Silva"; // Variavel do tipo texto
+
+  // Snackcase 
+  String nome_completo = "Carlos Souza"; // Variavel do tipo texto
+
+  //num - pode ser inteiro ou decimal
+  num peso = 70.5; // Variavel do tipo num 
+  num pesoInteiro = 70; // Variavel do tipo num
+
+  //var é definida automaticamente pelo tipo do valor atribuido
+  var cidade = "São Paulo"; // Variavel do tipo String
+  var temperatura = 25.5; // Variavel do tipo double
+  
+  Object objeto = "Isso é um objeto"; 
+  // A variavel do tipo Object pode armazenar qualquer tipo de dado
+  dynamic dinamico = 42; 
+  // A variavel do tipo dynamic pode armazenar qualquer tipo de dado e 
+  //pode ser alterada durante a execução do programa
+
+  // PALAVRAS RESERVADAS DART
+  // var, int, double, String, bool, num, Object, dynamic,
+  // void, if, else, for, while, switch, case, default, break, continue, 
+  // return, class, import, library, part, typedef, enum, mixin, extension, 
+  // this, super, new, const, final, static, abstract, async, await, yield, 
+  // is, as, in, on, try, catch, finally, throw, rethrow, assert, with, 
+  // implements, operator, covariant, deferred, export, external, factory, get, 
+
+}
