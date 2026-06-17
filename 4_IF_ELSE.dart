c89ac5e4d5aa9499937248dd6861a655da77975a
DaredevPy <renatopiment@gmail.com>
2026-06-17 12:00:59 -0300

    17/06/2026

---
+++ 4_IF_ELSE.dart
@@ -0,0 +1,11 @@
+void main () {
+  idade = 18;
+  if (idade > 18) {
+    print("Você é maior de idade.");
+    } else if (idade == 18) {
+      print("Você tem exatamente 18 anos.");
+    } else {
+      print("Você é menor de idade.");
+
+  }
+}
---
+++ 5_OPeradores_relacionais.dart
@@ -0,0 +1,33 @@
+void main () {
+
+  //Operadores Relacionais
+  //Temos 6 tipos de operadores relacionais:
+  // > (maior)
+  // < (menor)
+  // >= (maior ou igual)
+  // <= (menor ou igual)
+  // == (igual)
+  // != (diferente)
+  final idade = 18;
+  final TipoPet = 'Gato';
+
+  if (idade >=18) {
+    print('Você pode tirar Habilitação');
+  }
+  
+  if (idade > 17) {
+    print('Você pode tirar Habilitação');
+  }
+
+  if (idade >=18){
+    print('Você pode tirar Habilitação');
+  }
+
+  if (TipoPet != 'Cachorro') {
+    print('Desculpe, só aceitamos gatos como pet');
+
+
+    
+
+
+}
