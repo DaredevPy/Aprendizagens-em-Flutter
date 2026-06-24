84f1d010d4862721d5bc2f575f6726ff5d0b1928
DaredevPy <renatopiment@gmail.com>
2026-06-23 21:11:05 -0300

    23/06/2026

---
+++ 9_operador_de_coalescência_nula.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  print("Digite seu nome:");
+  String? entrada = stdin.readLineSync();
+
+  // Se entrada for null, usa "Usuário não identificado"
+  String nome = entrada ?? "Usuário não identificado";
+
+  print("Bem-vindo, $nome!");
+}
+//Caso o usuário não digite nada e apenas pressione Enter, 
+//a variável `entrada` será null, e o operador de coalescência nula (`??`) garantirá que
+// a variável `nome` receba o valor padrão "Usuário não identificado". 
