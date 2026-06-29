4b846c2ca23a006597f7aa91ec1dc4076d32d847
DaredevPy <renatopiment@gmail.com>
2026-06-29 12:01:46 -0300

    29/06/2026

--- 13_Manipulando_Listas.dart
+++ 13_Manipulando_Listas.dart
@@ -10,5 +10,41 @@ print(nomes);
 
 // acesando elementos da lista
 print(nomes[0]);
+print(nomes[2]);
+
+// alterando elementos da lista
+// neste caso substituindo o elemento do índice 1
+// mas levando todos os elementos para frente
+nomes.insert(0,'Ana Paula');
+
+// removendo elementos da lista
+nomes.remove('Carlos');
+
+// .addAll
+// adiciona uma lista dentro de outra lista
+final outrosNomes = ['Juliana', 'Pedro'];
+
+// removeWhere
+// remove todos os elementos que atendem a condição 
+nomes.removeWhere(nome)
+print('Nome procurado $none');
+if (nome == 'Daniel') {
+  return true;
+} else {
+  return false;
+
+  print(nomes);
+
+//contando a quantidade de indices
+print(nomes.length);
+
+//Pegando o primeiro item da lista 
+//print(nomes[0]);                  (alternativa)
+print(nomes.first);
+
+//Pegando o último item da lista
+//print([nomes.length - 1]);         (alternativa)
+print(nomes.last);
+
 
 }
